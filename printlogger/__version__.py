eb4370d9b2a4f795414b6214aeae3dfc33d63dbc
hnsong <root@hnsong-control.loopvm>
2022-02-28 14:04:30 +0900

    create __version__ file

---
+++ printlogger/__version__.py
@@ -0,0 +1,9 @@
+####
+__title__ = 'printlogger'
+__description__ = 'Color Print and logging'
+__url__ = 'https://github.com/HonamSong/printlogger'
+__version__ = '0.0.1'
+__author__ = 'Honam Song'
+__author_email__ = 'louky0714@gmail.com'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2022 hnsong'
